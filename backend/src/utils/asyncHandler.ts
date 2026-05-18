db5a00c76c403c5b100c8954c4d6be25eaa7b51d
Ishola Oluwademilade <161754581+Freeman-jnr@users.noreply.github.com>
2026-05-18 21:21:47 +0100

    Delete backend directory

--- backend/src/utils/asyncHandler.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export const asyncHandler =
-  <T extends Request = Request>(
-    fn: (req: T, res: Response, next: NextFunction) => Promise<unknown>
-  ) =>
-  (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req as T, res, next)).catch(next);
--- backend/src/utils/crypto.ts
+++
@@ -1,8 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const hashPassword = (plain: string) => bcrypt.hash(plain, 12);
-export const comparePassword = (plain: string, hash: string) =>
-  bcrypt.compare(plain, hash);
-
-export const generateOtp = (len = 6) =>
-  Array.from({ length: len }, () => Math.floor(Math.random() * 10)).join("");
